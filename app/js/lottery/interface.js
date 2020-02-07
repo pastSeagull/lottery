543906cb15f580297a2bd0c4f6a89c5de5855071
snpdcfg <2567570935@qq.com>
2020-02-07 22:57:24 +0800

    zz

---
+++ app/js/lottery/interface.js
@@ -0,0 +1,40 @@
+import $ from 'jquery';
+
+class Interface {
+  getOmit (issue) {
+    let self = this;
+    return new Promise((resolve, reject => {
+      $ajax({
+        url: '/get/omit',
+        data: {
+          issue: issue
+        },
+        dataType: 'json',
+        success: function (res) {
+          self.setOmit(res.data);
+          resolve.call(self, res);
+        },
+        error: function (err) {
+          reject.call(err);
+        }
+      })
+    }));
+  }
+
+  gitOpenCode (issue) {
+    let self = this;
+    return new Promise((resolve, reject) => {
+      $ajax({
+        url: '/get/pencode',
+        data: {
+          issue: issue
+        },
+        dataType: 'json',
+        success: function (res) {
+          self.setOpenCode(res.data);
+          resolve.call(self, res);
+        }
+      })
+    })
+  }
+}
